d5d59b0c5458161ee5584760ff9e0c96082e28d3
luis lopez <llopez@elcatador.com>
2023-09-21 11:45:59 -0400

    Cors update

--- ExcelAndWordApi/Program.cs
+++ ExcelAndWordApi/Program.cs
@@ -1,4 +1,18 @@
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.AllowAnyOrigin();
+                          policy.AllowAnyMethod();
+                          policy.AllowCredentials();
+                          policy.AllowAnyHeader();
+                      });
+});
 
 // Add services to the container.
 
@@ -17,6 +31,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
